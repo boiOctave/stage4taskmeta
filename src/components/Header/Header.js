19218ee1edf4bddb8e209b193c5a2c43764058b1
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-12 08:41:12 +0100

    style: made nav  responsive and added hamburger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
 
 import './Header.css';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleMenu = () => {
+        isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
+    };
     return (
         <header className='header '>
             <nav className='nav flex container'>
                 <a href='#' className='logoContent'>
                     <img src={logo} alt='logo' />
                 </a>
-                <ul className='nav-links flex'>
+                <ul className={`nav-links flex ${isMenuOpen ? 'open' : null}`}>
                     <li className='nav-link'>
                         <a href='#'>Home</a>
                     </li>
@@ -25,11 +30,29 @@ const Header = () => {
                     <li className='nav-link'>
                         <a href='#'>Community</a>
                     </li>
+                    <li>
+                        <div className='nav__button-container '>
+                            <button
+                                type='submit'
+                                className='button nav__button-mobile'>
+                                Connect Wallet
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        {' '}
+                        <div className='closeMenu' onClick={handleMenu}>
+                            <span class='material-symbols-outlined'>close</span>
+                        </div>
+                    </li>
                 </ul>
-                <div className='nav__button '>
-                    <button className='button nav__button'>
+                <div className='nav__button-container '>
+                    <button type='submit' className='button nav__button'>
                         Connect Wallet
                     </button>
+                </div>
+                <div className='hamburgerMenu' onClick={handleMenu}>
+                    <span className='material-symbols-outlined'>menu</span>
                 </div>
             </nav>
         </header>
