0d8aa0e2d8b26281054451ddd3789ef2436dfaa7
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-12 11:41:10 +0100

    feat: created hero section

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,15 @@
 import Header from 'components/Header/Header';
+import Hero from 'components/Hero/Hero';
 
 const Main = () => {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <main>
+                <Hero />
+            </main>
+        </>
+    );
 };
 
 export default Main;
