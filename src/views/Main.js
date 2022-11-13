20f5e1736425e19e9d8c3a2b8a99ca4490443b05
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-13 18:58:03 +0100

    feat:created nfts section

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,7 @@
 import Community from 'components/Community/Community';
 import Header from 'components/Header/Header';
 import Hero from 'components/Hero/Hero';
+import NFTs from 'components/NFTs/NFTs';
 import Wallets from '../components/Wallets/Wallets';
 
 const Main = () => {
@@ -11,6 +12,7 @@ const Main = () => {
                 <Hero />
                 <Wallets />
                 <Community />
+                <NFTs />
             </main>
         </>
     );
