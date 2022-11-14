7d6cc4d07698877b8eb33a7af03cb13589ed6a73
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-14 03:18:11 +0100

    feat:Added components to place route

--- src/views/PlaceToStay/PlaceToStay.js
+++ src/views/PlaceToStay/PlaceToStay.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Community from 'components/Community/Community';
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
+
 const PlaceToStay = () => {
-    return <div>PlaceToStay</div>;
+    return (
+        <>
+            <Header />
+            <main>
+                <Community />
+
+                <Footer />
+            </main>
+        </>
+    );
 };
 
 export default PlaceToStay;
