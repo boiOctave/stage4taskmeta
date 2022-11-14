dd8e6e72e08760441b1b4d4891cce7746e831fbf
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-14 02:52:42 +0100

    style: creted blur effect foor header on scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 
 import './Header.css';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [windowPosition, setWindowPosition] = useState(0);
+
+    useEffect(() => {
+        window.onscroll = () => {
+            setWindowPosition(window.scrollY);
+        };
+        if (windowPosition >= 60) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    }, [windowPosition]);
+
     const handleMenu = () => {
         isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
     };
     return (
-        <header className='header '>
+        <header className={`header ${isScrolled ? 'inverted' : ''}`}>
             <nav className='nav flex container'>
                 <a href='#' className='logoContent'>
                     <img src={logo} alt='logo' />
                 </a>
-                <ul className={`nav-links flex ${isMenuOpen ? 'open' : null}`}>
-                    <li className='nav-link'>
+                <ul className={`nav-links flex ${isMenuOpen ? 'open' : ''}`}>
+                    <li
+                        className='nav-link'
+                        onClick={() => setIsMenuOpen(false)}>
                         <a href='#'>Home</a>
                     </li>
 
-                    <li className='nav-link'>
-                        <a href='/place'>Place to stay</a>
+                    <li
+                        className='nav-link'
+                        onClick={() => setIsMenuOpen(false)}>
+                        <Link to='/place-to-stay'>Place to stay</Link>
                     </li>
 
-                    <li className='nav-link'>
+                    <li
+                        className='nav-link'
+                        onClick={() => setIsMenuOpen(false)}>
                         <a href='#nfts'>NFTs</a>
                     </li>
 
-                    <li className='nav-link'>
-                        <a href='#comunity'>Community</a>
+                    <li
+                        className='nav-link'
+                        onClick={() => setIsMenuOpen(false)}>
+                        <a href='#community'>Community</a>
                     </li>
                     <li>
                         <div className='nav__button-container '>
@@ -42,7 +65,9 @@ const Header = () => {
                     <li>
                         {' '}
                         <div className='closeMenu' onClick={handleMenu}>
-                            <span class='material-symbols-outlined'>close</span>
+                            <span className='material-symbols-outlined'>
+                                close
+                            </span>
                         </div>
                     </li>
                 </ul>
