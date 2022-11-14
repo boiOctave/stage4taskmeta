be4c0ec96d4efcbb7a57bd8be2b321f42bbfb432
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-14 03:34:04 +0100

    feat: created array for new route data

--- src/data/Data.js
+++ src/data/Data.js
@@ -6,6 +6,14 @@ import house5 from '../assets/images/Frame 151house5.svg';
 import house6 from '../assets/images/Frame 151house6.svg';
 import house7 from '../assets/images/Frame 151house7.svg';
 import house8 from '../assets/images/Frame 151house8.svg';
+import house9 from '../assets/images/Frame 151house9.svg';
+import house10 from '../assets/images/Frame 151house10.png';
+import house11 from '../assets/images/Frame 151house11.svg';
+import house12 from '../assets/images/Frame 151house12.png';
+import house13 from '../assets/images/Frame 151house13.svg';
+import house14 from '../assets/images/Frame 151house14.svg';
+import house15 from '../assets/images/Frame 151house15.svg';
+import house16 from '../assets/images/Frame 151house16.svg';
 
 export const homeData = [
     {
@@ -59,6 +67,121 @@ export const homeData = [
     },
     {
         img: house8,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+];
+
+export const placesData = [
+    {
+        img: house5,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house6,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house7,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house8,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house1,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house2,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house3,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house4,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house9,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house10,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house11,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house12,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house13,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house14,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house15,
+        name: 'Desert King',
+        distance: 2345,
+        price: 1,
+        length: 2,
+    },
+    {
+        img: house16,
         name: 'Desert King',
         distance: 2345,
         price: 1,
