e9f17201353d07bf9d51d6baca1f1448078fa6df
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-14 03:45:30 +0100

    refactor: passed places data into community component in new route

--- src/views/PlaceToStay/PlaceToStay.js
+++ src/views/PlaceToStay/PlaceToStay.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Community from 'components/Community/Community';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+import { placesData } from 'data/Data';
 
 const PlaceToStay = () => {
     return (
         <>
             <Header />
             <main>
-                <Community />
+                <Community data={placesData} />
 
                 <Footer />
             </main>
