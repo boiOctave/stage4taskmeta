202c157d5d87f8f7669e24b13d071c2c06439111
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-14 06:12:50 +0100

    feat: automatic scroll to top when route changes

--- src/views/Main.js
+++ src/views/Main.js
@@ -4,9 +4,22 @@ import Header from 'components/Header/Header';
 import Hero from 'components/Hero/Hero';
 import NFTs from 'components/NFTs/NFTs';
 import Wallets from '../components/Wallets/Wallets';
-
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { homeData } from 'data/Data';
 const Main = () => {
+    // ==============================|| NAVIGATION SCROLL TO TOP ||============================== //
+
+    const location = useLocation();
+    const { pathname } = location;
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
+    }, [pathname]);
     return (
         <>
             <Header />
--- src/views/PlaceToStay/PlaceToStay.js
+++ src/views/PlaceToStay/PlaceToStay.js
@@ -5,7 +5,22 @@ import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import { placesData } from 'data/Data';
 
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 const PlaceToStay = () => {
+    // ==============================|| NAVIGATION SCROLL TO TOP ||============================== //
+
+    const location = useLocation();
+    const { pathname } = location;
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
+    }, [pathname]);
     return (
         <>
             <Header />
